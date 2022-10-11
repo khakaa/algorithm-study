9d796f47d5344b9043403adba2743f65ffbc7147
khakaa <phr0210@naver.com>
2022-10-11 12:26:33 +0900

    [ADD] 백준 수들의 합 풀이

---
+++ 박하린/백준/Greedy/수들의 합.py
@@ -0,0 +1,14 @@
+N = int(input())
+count = 0
+total = 0
+
+for i in range(1, N+1):
+    total += i
+    count += 1
+
+    # 200의 경우 count=18, total=171인데, 200 - 179값을 더해주면 200이 되므로 답은 19가되는 원리
+    if total > N:
+        count -= 1
+        break
+
+print(count)
